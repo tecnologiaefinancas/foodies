08308f6b187a2305f5fa2e468274ea31b8d92896
Natalia Feitosa <estudandocertificacoes@gmail.com>
2025-04-07 14:50:07 -0300

    Foodies FrontEnd

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Menubar from './components/Menubar/Menubar';
+
+const App = () => {
+  return (
+    <div>
+      <Menubar/>
+    </div>
+  )
+}
+
+export default App
