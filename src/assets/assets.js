9f327ee9badae5d1fc5575b460b3840ee3f18674
Natalia Feitosa <estudandocertificacoes@gmail.com>
2025-04-10 19:23:14 -0300

    Add categories photos

---
+++ src/assets/assets.js
@@ -0,0 +1,13 @@
+import pizza from './pizza.png';
+import icecream from './icecream.png';
+
+export const categories = [
+    {
+        category: 'Ice Creams',
+        icon: icecream
+    },
+    {
+        category: 'Pizza',
+        icon: pizza
+    }
+];
